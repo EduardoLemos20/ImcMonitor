af46660508f37681a0b5784e4e600657533ba249
Eduardo Lemos <eduardolemos1606@gmail.com>
2020-09-16 23:21:30 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) throws Exception {
+
+    // TesteSimples teste = new TesteSimples();
+    TesteUnitario tu = new TesteUnitario();
+    tu.executarAll();
+
+    System.out.println("\n\nimcmonitor, Fim!");
+  }
+}
---
+++ teste/TestSimples.java
@@ -0,0 +1,11 @@
+class TestSimples {
+
+  public void executar() {
+
+    Atleta atleta = new Atleta();
+    atleta.setNome("Piedade Silva");
+    atleta.setIdade(36);
+
+    System.out.println("Nome = " + atleta.getNome());
+    System.out.println("Idade = " + atleta.getIdade());
+  }
